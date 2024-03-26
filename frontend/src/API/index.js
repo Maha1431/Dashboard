605fdc0912e3d4fb92040d30547896d5dfe9820b
Mahalakshmi <mmahaece07@gmail.com>
2024-03-26 23:34:04 +0530

    UI Tested

--- frontend/src/API/index.js
+++ frontend/src/API/index.js
@@ -1,18 +1,18 @@
 export const getOrders = () => {
-  return fetch("http://localhost:5000/carts/1").then((res) => res.json());
+  return fetch("https://dashboard-8hzz.onrender.com/carts/1").then((res) => res.json());
 };
 
 export const getRevenue = () => {
-  return fetch("http://localhost:5000/carts").then((res) => res.json());
+  return fetch("https://dashboard-8hzz.onrender.com/carts").then((res) => res.json());
 };
 
 export const getInventory = () => {
-  return fetch("http://localhost:5000/products").then((res) => res.json());
+  return fetch("https://dashboard-8hzz.onrender.com/products").then((res) => res.json());
 };
 
 export const getCustomers = () => {
-  return fetch("http://localhost:5000/users").then((res) => res.json());
+  return fetch("https://dashboard-8hzz.onrender.com/users").then((res) => res.json());
 };
 export const getComments = () => {
-  return fetch("http://localhost:5000/comments").then((res) => res.json());
+  return fetch("https://dashboard-8hzz.onrender.com/comments").then((res) => res.json());
 };
